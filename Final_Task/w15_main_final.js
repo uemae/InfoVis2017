7c45cc052f8ef22cbab605a686109eb749868d20
植前貴大 <uemae@ip251-161.wlan.kobe-u.ac.jp>
2017-06-16 10:49:37 +0900

    Initial commit

---
+++ Final_Task/w15_main_final.js
@@ -0,0 +1,44 @@
+function main()
+{
+    var volume = new KVS.LobsterData();
+    var screen = new KVS.THREEScreen();
+    var ch = 1.0;
+
+    screen.init( volume, {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        enableAutoResize: false
+    });
+
+    var bounds = Bounds( volume );
+    screen.scene.add( bounds );
+
+    var name = document.getElementById('range');
+    var isovalue = range.value;
+    var name = document.getElementById('color');
+    var color_a = color.value;
+    var surfaces = Isosurfaces( volume, isovalue, screen, ch, color_a );
+    screen.scene.add( surfaces );
+
+      document.getElementById('btn').addEventListener('click',function(){
+        var range = document.getElementById('range');
+        isovalue = range.value;
+        name = document.getElementById('select');
+        ch = select.value;
+        name = document.getElementById('color');
+        color_a = color.value;
+        screen.scene.remove(surfaces);
+        surfaces = Isosurfaces( volume, isovalue, screen, ch, color_a );
+        screen.scene.add(surfaces);
+      },false);
+
+    document.addEventListener( 'mousemove', function() {
+        screen.light.position.copy( screen.camera.position );
+    });
+
+    window.addEventListener( 'resize', function() {
+        screen.resize( [ window.innerWidth, window.innerHeight ] );
+    });
+
+    screen.loop();
+}
